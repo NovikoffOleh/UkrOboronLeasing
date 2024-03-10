bd1a8eb579104bf673c02fae009107f14e059410
Oleh Novikov <novikoff10000@gmail.com>
2024-03-10 19:16:23 +0200

    correct

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.bike-slider, .slider__items').slick({
+    arrows:false,
+    dots: true,
+    fade:true,
+    autoplay:true,
+    autoplaySpeed:2000,
+  });
+})
+//! отключение стрелок у слайдера
+//arrows:false,
+//dots: true
+//! управление воспроизведением у слайдера
+    //fade:true,
+    //autoplay:true,
+    //autoplaySpeed:2000,
